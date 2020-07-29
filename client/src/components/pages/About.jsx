e78c5035cdbe69e557ec9d0cacdcef2d9b88226d
Mark <mark.boyd1414@gmail.com>
2020-07-29 10:00:20 -0400

    Fixed about page

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-function About () {
+function About() {
   return (
-    <Container className="my-5">
-      <header class="jumbotron">
+    <Container className='my-5'>
+      <header class='jumbotron'>
         <h1>All About Me</h1>
       </header>
 
       <div>
-        <p>The autobiography of Shaun McKinnon</p>
+        <p>The autobiography of Mark Boyd</p>
       </div>
     </Container>
   );
